ba5a6c49286f9600e27be45d6990d9a74b88dacb
Ehsan Elgendi <ehsan.elgendi@gmail.com>
2022-10-20 16:46:05 +0000

    adding constants module

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, MOVE_LEFT_KEY } = require("./constants");
+
 let connection;
 
 const setupInput = function (conn) {
@@ -14,17 +16,17 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
-  if (key === 'a') {
-    connection.write("Move: left");
-  }
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
+  }
+  if (key === MOVE_LEFT_KEY) {
+    connection.write("Move: left");
   }
   if (key === 'h') {
     connection.write("Say: Hello!");
